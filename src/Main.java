a343e8447fea7cf692ce843087f37c587eaaa576
qixuefeng <xfseven7@foxmail.com>
2019-01-23 23:13:58 +0800

    just do it!

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.annotation.processing.FilerException;
 import java.io.*;
 import java.util.*;
 import java.util.regex.Matcher;
@@ -5,16 +6,77 @@ import java.util.regex.Pattern;
 
 public class Main {
 
-
-    public static String src_name = "src.txt";// 字幕文件
-    public static String dst_name = "dst.txt";// 台词文件
-
     public static void main(String[] args) {
 
-        // 字幕文件
-        File src = new File(src_name);
-        // 提取台词后的文件
-        File dst = new File(dst_name);
+        long l = System.currentTimeMillis();
+
+        // 使用穷举发写代码是为了方便控制
+
+        // 查理成长日记第一季 统计20集
+        ArrayList<String> strings1 = doIt("1.txt", "dst_1.txt");
+        ArrayList<String> strings2 = doIt("2.txt", "dst_2.txt");
+        ArrayList<String> strings3 = doIt("3.txt", "dst_3.txt");
+        ArrayList<String> strings4 = doIt("4.txt", "dst_4.txt");
+        ArrayList<String> strings5 = doIt("5.txt", "dst_5.txt");
+        ArrayList<String> strings6 = doIt("6.txt", "dst_6.txt");
+        ArrayList<String> strings7 = doIt("7.txt", "dst_7.txt");
+        ArrayList<String> strings8 = doIt("8.txt", "dst_8.txt");
+        ArrayList<String> strings9 = doIt("9.txt", "dst_9.txt");
+        ArrayList<String> strings10 = doIt("10.txt", "dst_10.txt");
+        ArrayList<String> strings11 = doIt("11.txt", "dst_11.txt");
+        ArrayList<String> strings12 = doIt("12.txt", "dst_12.txt");
+        ArrayList<String> strings13 = doIt("13.txt", "dst_13.txt");
+        ArrayList<String> strings14 = doIt("14.txt", "dst_14.txt");
+        ArrayList<String> strings15 = doIt("15.txt", "dst_15.txt");
+        ArrayList<String> strings19 = doIt("19.txt", "dst_19.txt");
+        ArrayList<String> strings22 = doIt("22.txt", "dst_22.txt");
+        ArrayList<String> strings23 = doIt("23.txt", "dst_23.txt");
+        ArrayList<String> strings24 = doIt("24.txt", "dst_24.txt");
+        ArrayList<String> strings25 = doIt("25.txt", "dst_25.txt");
+
+
+        strings1.addAll(strings2);
+        strings1.addAll(strings3);
+        strings1.addAll(strings4);
+        strings1.addAll(strings5);
+        strings1.addAll(strings6);
+        strings1.addAll(strings7);
+        strings1.addAll(strings8);
+        strings1.addAll(strings9);
+        strings1.addAll(strings10);
+        strings1.addAll(strings11);
+        strings1.addAll(strings12);
+        strings1.addAll(strings13);
+        strings1.addAll(strings14);
+        strings1.addAll(strings15);
+        strings1.addAll(strings19);
+        strings1.addAll(strings22);
+        strings1.addAll(strings23);
+        strings1.addAll(strings24);
+        strings1.addAll(strings25);
+
+
+//         头脑特工队和超人总动员2
+//        ArrayList<String> src1 = doIt("src.txt", "dst.txt");
+//        ArrayList<String> src2 = doIt("src1.txt", "dst1.txt");
+//        src1.addAll(src2);
+
+        Map<String, Integer> stringIntegerMap = list2map(strings1);
+
+        printMap(stringIntegerMap);
+
+        result(strings1, stringIntegerMap);
+
+        // 没有优化代码，目的很单纯，所以运行速度不快，反正要的不是效率，就算10秒钟才执行出来也没关系。
+        // 优化应该会使用到线程池
+        System.out.println("时间 = " + (System.currentTimeMillis() - l));
+
+    }
+
+    private static ArrayList<String> doIt(String src_name, String dst_name) {
+
+        File src = new File("before/" + src_name);
+        File dst = new File("after/" + dst_name);
 
         saveNewFile(src, dst);
 
@@ -32,10 +94,7 @@ public class Main {
             }
         });
 
-        Map<String, Integer> sortedMap = list2map(wordList);
-        printMap(sortedMap);
-
-        result(wordList, sortedMap);
+        return wordList;
 
     }
 
@@ -93,7 +152,7 @@ public class Main {
      */
     private static void printMap(Map<String, Integer> map) {
         for (Map.Entry entry : map.entrySet()) {
-            System.out.println(entry.getKey() + "," + entry.getValue());
+            System.out.println(entry.getKey() + "：" + entry.getValue());
         }
     }
 
